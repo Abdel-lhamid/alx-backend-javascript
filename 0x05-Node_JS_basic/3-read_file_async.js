e3f13fc873f3f4fc37d6abc4dc0d023be53f893e
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-10-01 13:59:18 +0100

    task 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+function countStudents(fileName) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter(line => line.trim() !== '');
+      const students = lines.slice(1);
+      const numberOfStudents = students.length;
+
+      console.log(`Number of students: ${numberOfStudents}`);
+
+      const fields = {};
+      students.forEach(student => {
+        const [firstname, , , field] = student.split(',');
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      });
+
+      for (const [field, names] of Object.entries(fields)) {
+        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
