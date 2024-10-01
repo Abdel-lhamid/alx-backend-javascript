7c5c011745a099ed4401f5703b1fdb89a074101b
ABDELHAMID El Yaagoubi <abdelhamidbusiness@gmail.com>
2024-10-01 13:45:32 +0100

    just said it

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,16 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-if (process.argv.length > 2) {
-  const name = process.argv[2];
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
 } else {
   process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write('This important software is now closing\n');
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
   });
 }
